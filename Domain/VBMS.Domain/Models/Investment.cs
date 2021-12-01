6215e2538a5aa31983c7644116387e40d750bab8
Wisdom Jere <wisdomjere5@gmail.com>
2021-12-01 12:26:25 +0200

    Update models

--- Domain/VBMS.Domain/Models/Investment.cs
+++ Domain/VBMS.Domain/Models/Investment.cs
@@ -1,6 +1,13 @@
-﻿namespace VBMS.Domain.Models
+﻿namespace VBMS.Domain.Models;
+
+public class Investment : Entity<int>
 {
-    public class Investment : Entity<int>
-    {
-    }
+    public int MemberId { get; set; }
+
+    public DateTime DateInvested { get; set; }
+
+    public int InvestmentPeriodId { get; set; }
+
+    public InvestmentPeriod InvestmentPeriod { get; set; }
+
 }
--- Domain/VBMS.Domain/Models/InvestmentPeriod.cs
+++ Domain/VBMS.Domain/Models/InvestmentPeriod.cs
@@ -1,6 +1,6 @@
 ﻿namespace VBMS.Domain.Models;
 
-public class MonthlyInvestmentAmount : AuditableEntity<int>
+public class InvestmentPeriod : AuditableEntity<int>
 {
     [DataType(DataType.Date)]
     public DateTime BeginDate { get; set; }
