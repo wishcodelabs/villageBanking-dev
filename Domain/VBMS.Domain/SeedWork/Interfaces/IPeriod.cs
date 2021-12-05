cf8ad05ab0a8b34207770a5a706dab496d4df573
Wisdom Jere <wisdomjere5@gmail.com>
2021-12-05 08:51:12 +0200

    Update Periods

---
+++ Domain/VBMS.Domain/SeedWork/Interfaces/IPeriod.cs
@@ -0,0 +1,10 @@
+﻿namespace VBMS.Domain.SeedWork;
+
+public interface IPeriod : IAuditableEntity
+{
+    [DataType(DataType.Date)]
+    public DateTime BeginDate { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime EndDate { get; set; }
+}
---
+++ Domain/VBMS.Domain/SeedWork/Period.cs
@@ -0,0 +1,10 @@
+﻿namespace VBMS.Domain.SeedWork;
+
+public class Period : AuditableEntity<int>, IPeriod
+{
+    [DataType(DataType.Date)]
+    public DateTime BeginDate { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime EndDate { get; set; }
+}
