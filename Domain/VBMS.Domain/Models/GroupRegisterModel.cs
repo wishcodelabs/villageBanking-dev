bded9ead9879fa1292c13f5b2e6f143655e31658
Wisdom Jere <wisdomjere5@gmail.com>
2021-12-25 17:51:56 +0200

    Bind Properties

--- Domain/VBMS.Domain/Models/GroupRegisterModel.cs
+++ Domain/VBMS.Domain/Models/GroupRegisterModel.cs
@@ -2,17 +2,23 @@
 {
     public class GroupRegisterModel
     {
-        [Required]
+        [Required(ErrorMessage = "Group name is required")]
         public string GroupName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "First name is required")]
         public string FirstName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Last name is required")]
         public string LastName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Email address is required")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Phone number is required")]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Group role required")]
+        public VillageGroupRole Role { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
@@ -20,6 +26,7 @@
 
 
         [Required]
+        [Compare(nameof(Password), ErrorMessage = "Confirm password and password should match")]
         public string ConfirmPassword { get; set; }
     }
 }
