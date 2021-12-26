56cbe03f4dc79751e327b4c86cf3e5eb6982ec0b
Wisdom Jere <wisdomjere5@gmail.com>
2021-12-26 16:27:20 +0200

    Add register request

--- Domain/VBMS.Domain/Requests/RegisterRequest.cs
+++ Domain/VBMS.Domain/Requests/RegisterRequest.cs
@@ -2,5 +2,36 @@
 {
     public class RegisterRequest
     {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public bool AutoConfirm { get; set; }
+
+        public bool IsValid { get; set; }
+
     }
 }
--- Infrastructure/VBMS.Infrastructure/Services/Identity/UserService.cs
+++ Infrastructure/VBMS.Infrastructure/Services/Identity/UserService.cs
@@ -1,6 +1,4 @@
-﻿
-
-namespace VBMS.Infrastructure.Services.Identity
+﻿namespace VBMS.Infrastructure.Services.Identity
 {
     public class UserService : IUserService
     {
@@ -27,6 +25,28 @@ namespace VBMS.Infrastructure.Services.Identity
         public Task<Result> RegisterAsync(RegisterRequest request)
         {
             throw new NotImplementedException();
+        }
+        private User CreateUser()
+        {
+            try
+            {
+                return Activator.CreateInstance<User>();
+            }
+            catch
+            {
+                throw new InvalidOperationException($"Can't create an instance of '{nameof(User)}'. " +
+                    $"Ensure that '{nameof(User)}' is not an abstract class and has a parameterless constructor, or alternatively " +
+                    $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
+            }
+        }
+
+        private IUserEmailStore<User> GetEmailStore()
+        {
+            if (!userManager.SupportsUserEmail)
+            {
+                throw new NotSupportedException("The default UI requires a user store with email support.");
+            }
+            return (IUserEmailStore<User>)userStore;
         }
     }
 }
