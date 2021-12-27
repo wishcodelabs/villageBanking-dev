db7a245ecede6e440319bbc243715d633c7e13b9
Wisdom Jere <wisdomjere5@gmail.com>
2021-12-27 14:20:06 +0200

    More Services

---
+++ Domain/VBMS.Domain/Requests/ResetPasswordRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace VBMS.Domain.Requests
+{
+    public class ResetPasswordRequest
+    {
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string NewPassword { get; set; }
+
+        [Required]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}
---
+++ Infrastructure/VBMS.Infrastructure/Interfaces/IUserService.cs
@@ -0,0 +1,19 @@
+﻿namespace VBMS.Infrastructure.Interfaces
+{
+    public interface IUserService : IIdentityService
+    {
+        Task<int> GetCount();
+
+        Task<string> GetUserName(int userId);
+
+        Task<User> GetUserAsync(int userId);
+
+        Task<List<User>> GetAllUsers();
+
+        Task<List<Role>> GetRoles(int userId);
+
+        Task<List<RoleClaim>> GetRoleClaims(int userId);
+
+        Task<Result> ResetPassword(ResetPasswordRequest request);
+    }
+}
