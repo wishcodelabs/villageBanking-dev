fcdbfe07aeb8f03ee89a51b134c7de16a109921a
Wisdom Jere <wisdomjere5@gmail.com>
2021-12-30 16:30:14 +0200

    more implementations

--- Infrastructure/VBMS.Infrastructure/Interfaces/IDashboardService.cs
+++ Infrastructure/VBMS.Infrastructure/Interfaces/IDashboardService.cs
@@ -5,11 +5,11 @@
         //Chat Data
         //Analysis Data etc
         Task<int> GetNewLoanApplications(int groupId, int investmentPeriodId);
-        Task<int> GetTotalShares(int groupId, int investmentPeriodId);
-        Task<double> GetTotalInvestments(int groupId, int investmentPeriodId);
+        Task<double> GetTotalShares(int groupId, int investmentPeriodId);
+        Task<decimal> GetTotalInvestments(int groupId, int investmentPeriodId);
         Task<int> GetBadEggs(int groupId, int investmentPeriodId);
 
-        Task<List<double>> GetMonthlyInvestments(int groupId, int investmentPeriodId);
+        Task<double> GetMonthlyInvestments(int groupId, int investmentPeriodId, int month);
 
         Task<List<int>> GetMonthlyLoanApplications(int groupId, int investmentPeriodId);
 
--- Infrastructure/VBMS.Infrastructure/Services/Analysis/DashboardService.cs
+++ Infrastructure/VBMS.Infrastructure/Services/Analysis/DashboardService.cs
@@ -13,9 +13,15 @@
             throw new NotImplementedException();
         }
 
-        public Task<List<double>> GetMonthlyInvestments(int groupId, int investmentPeriodId)
+        public async Task<double> GetMonthlyInvestments(int groupId, int investmentPeriodId, int month)
         {
-            throw new NotImplementedException();
+            var investments = await context.Set<Investment>()
+                                                .Include(s => s.Investor)
+                                                .Where(i => i.InvestmentPeriodId == investmentPeriodId && i.Investor.VillageGroupId == groupId && i.DateInvested.Month == month)
+                                                .ToListAsync();
+            var total = 0.0;
+            investments.ForEach(x => { total += ((double)x.AmountInvested); });
+            return total;
         }
 
         public Task<List<int>> GetMonthlyLoanApplications(int groupId, int investmentPeriodId)
@@ -28,9 +34,14 @@
             throw new NotImplementedException();
         }
 
-        public Task<int> GetNewLoanApplications(int groupId, int investmentPeriodId)
+        public async Task<int> GetNewLoanApplications(int groupId, int investmentPeriodId)
         {
-            throw new NotImplementedException();
+            var loans = await context.Set<Loan>()
+                                        .Include(l => l.Applicant)
+                                        .ThenInclude(a => a.GroupMembership)
+                                        .Where(l => l.DateSubmitted.Day == DateTime.Today.Day && l.Applicant.GroupMembership.VillageGroupId == groupId && l.Status == Status.Pending)
+                                        .ToListAsync();
+            return loans.Count;
         }
 
         public Task<List<int>> GetOverallPerf(int groupId, int investmentPeriodId)
@@ -38,14 +49,27 @@
             throw new NotImplementedException();
         }
 
-        public Task<double> GetTotalInvestments(int groupId, int investmentPeriodId)
+        public async Task<decimal> GetTotalInvestments(int groupId, int investmentPeriodId)
         {
-            throw new NotImplementedException();
+            var total = 0.0M;
+            var investments = await context.Set<Investment>()
+                                                    .Include(s => s.Investor)
+                                                    .Where(i => i.InvestmentPeriodId == investmentPeriodId && i.Investor.VillageGroupId == groupId)
+                                                    .ToListAsync();
+            investments.ForEach(x => { total += x.AmountInvested; });
+            return total;
+
         }
 
-        public Task<int> GetTotalShares(int groupId, int investmentPeriodId)
+        public async Task<double> GetTotalShares(int groupId, int investmentPeriodId)
         {
-            throw new NotImplementedException();
+            var shares = await context.Set<VillageGroupMemberShare>()
+                                        .Include(s => s.Shareholder)
+                                        .Where(s => s.InvestmentPeriodId == investmentPeriodId && s.Shareholder.VillageGroupId == groupId)
+                                        .ToListAsync();
+            var total = 0.0;
+            shares.ForEach(s => { total += s.NumberOfShares; });
+            return total;
         }
     }
 }
