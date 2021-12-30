be4726b2d243ca690990d82b673727b9f7db4856
Wisdom Jere <wisdomjere5@gmail.com>
2021-12-30 15:25:21 +0200

    Implement Dashboard Service

--- Infrastructure/VBMS.Infrastructure/Interfaces/IDashboardService.cs
+++ Infrastructure/VBMS.Infrastructure/Interfaces/IDashboardService.cs
@@ -4,5 +4,17 @@
     {
         //Chat Data
         //Analysis Data etc
+        Task<int> GetNewLoanApplications(int groupId);
+        Task<int> GetTotalShares(int groupId);
+        Task<double> GetTotalInvestments(int groupId);
+        Task<int> GetBadEggs(int groupId);
+
+        Task<List<double>> GetMonthlyInvestments(int groupId);
+
+        Task<List<int>> GetMonthlyLoanApplications(int groupId);
+
+        Task<List<double>> GetMonthlyRevenue(int groupId);
+        Task<List<int>> GetOverallPerf(int groupId);
+
     }
 }
--- Infrastructure/VBMS.Infrastructure/Services/Analysis/DashboardService.cs
+++ Infrastructure/VBMS.Infrastructure/Services/Analysis/DashboardService.cs
@@ -7,5 +7,45 @@
         {
             context = dbContext;
         }
+
+        public Task<int> GetBadEggs(int groupId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<double>> GetMonthlyInvestments(int groupId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<int>> GetMonthlyLoanApplications(int groupId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<double>> GetMonthlyRevenue(int groupId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetNewLoanApplications(int groupId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<int>> GetOverallPerf(int groupId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<double> GetTotalInvestments(int groupId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetTotalShares(int groupId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
