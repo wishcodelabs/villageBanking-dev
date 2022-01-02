4ef1dc2ab6c6b0998312f20578e5f74da01a2cec
Wisdom Jere <wisdomjere5@gmail.com>
2022-01-02 10:23:42 +0200

    Filter Implemented

--- Infrastructure/VBMS.Infrastructure/Services/Application/InvestmentService.cs
+++ Infrastructure/VBMS.Infrastructure/Services/Application/InvestmentService.cs
@@ -9,5 +9,13 @@
         {
             return await Repository.Entities().OrderByDescending(i => i.DateInvested).Where(i => i.Investor.VillageGroupId == groupId).ToListAsync();
         }
+        public async Task<List<Investment>> GetByPeriod(int period, int groupId)
+        {
+            if (period == 0)
+            {
+                return await GetByGroup(groupId);
+            }
+            return await Repository.Entities().OrderByDescending(i => i.DateInvested).Where(i => i.Investor.VillageGroupId == groupId && i.InvestmentPeriodId == period).ToListAsync();
+        }
     }
 }
