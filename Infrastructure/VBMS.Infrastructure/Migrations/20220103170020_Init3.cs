a1fbae5c6a2a13ac95277c49b239c969aee65240
Wisdom Jere <wisdomjere5@gmail.com>
2022-01-03 19:10:07 +0200

    Another Migration

---
+++ Infrastructure/VBMS.Infrastructure/Migrations/20220103170020_Init3.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VBMS.Infrastructure.Migrations
+{
+    public partial class Init3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApprovedBy",
+                table: "Investments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApprovedBy",
+                table: "Investments");
+        }
+    }
+}
