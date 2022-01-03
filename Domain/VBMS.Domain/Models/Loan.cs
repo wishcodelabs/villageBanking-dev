6e42ce08fdab3dbf0dbbc94c4444691514ba0bab
Wisdom Jere <wisdomjere5@gmail.com>
2022-01-03 16:31:35 +0200

    Update Loan

--- Domain/VBMS.Domain/Models/Loan.cs
+++ Domain/VBMS.Domain/Models/Loan.cs
@@ -2,26 +2,31 @@
 {
     public class Loan : AuditableEntity<int>
     {
-        public int ApplicationId { get; set; }
+        public int RequestId { get; set; }
 
         public decimal ApprovedAmount { get; set; }
+        public int PeriodId { get; set; }
 
         public DateTime DateDue { get; set; }
+
         public DateTime DateApproved { get; set; }
 
         public LoanStatus Status { get; set; }
 
         public int ApproverId { get; set; }
 
-        public int LoanTypeId { get; set; }
+        public int RatingId { get; set; }
 
         public string Details { get; set; }
-        public virtual LoanType LoanType { get; set; }
 
-        [ForeignKey(nameof(ApplicationId))]
+        [ForeignKey(nameof(RequestId))]
         public virtual LoanApplication ApplicationRequest { get; set; }
-        [ForeignKey(nameof(ApplicationId))]
+        [ForeignKey(nameof(RequestId))]
         public virtual VillageGroupMembership Approver { get; set; }
+        [ForeignKey(nameof(PeriodId))]
+        public virtual InvestmentPeriod Period { get; set; }
+        [ForeignKey(nameof(RatingId))]
+        public virtual LoanInterestRate InterestRate { get; set; }
         public virtual List<LoanPayment> Payments { get; set; }
 
     }
