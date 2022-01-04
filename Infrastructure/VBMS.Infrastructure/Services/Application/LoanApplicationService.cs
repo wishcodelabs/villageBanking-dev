56a5659c406d6682b2f915cde59c3d3f8350d9d8
Wisdom Jere <wisdomjere5@gmail.com>
2022-01-04 21:47:27 +0200

    More Filters

--- Infrastructure/VBMS.Infrastructure/Services/Application/LoanApplicationService.cs
+++ Infrastructure/VBMS.Infrastructure/Services/Application/LoanApplicationService.cs
@@ -5,5 +5,33 @@
         public LoanApplicationService(IUnitOfWork<int> _unitOfWork) : base(_unitOfWork)
         {
         }
+        public async Task<List<LoanApplication>> GetAllByUserGuid(Guid userGuid, int periodid)
+        {
+            var myApplications = new List<LoanApplication>();
+            if (periodid == 0)
+            {
+                myApplications = await Repository.Entities().Where(l => l.Applicant.GroupMembership.UserGuid == userGuid).ToListAsync();
+            }
+            else
+            {
+                myApplications = await Repository.Entities().Where(l => l.Applicant.GroupMembership.UserGuid == userGuid && l.PeriodId == periodid).ToListAsync();
+            }
+
+            return myApplications;
+        }
+        public async Task<List<LoanApplication>> GetAllByPeriod(int periodId, int groupId)
+        {
+            var list = new List<LoanApplication>();
+            if (periodId == 0)
+            {
+                list = await Repository.Entities().Where(l => l.Applicant.GroupMembership.VillageGroupId == groupId).ToListAsync();
+            }
+            else
+            {
+                list = await Repository.Entities().Where(l => l.Applicant.GroupMembership.VillageGroupId == groupId && l.PeriodId == periodId).ToListAsync();
+            }
+
+            return list;
+        }
     }
 }
