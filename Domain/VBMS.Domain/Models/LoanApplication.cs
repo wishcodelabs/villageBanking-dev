b25e0b46fc473bfa0f4801ac586124845417a732
Wisdom Jere <wisdomjere5@gmail.com>
2022-01-04 20:12:49 +0200

    Update Model

--- Domain/VBMS.Domain/Models/LoanApplication.cs
+++ Domain/VBMS.Domain/Models/LoanApplication.cs
@@ -4,6 +4,7 @@
     {
         public int ApplicantId { get; set; }
 
+        public int PeriodId { get; set; }
         public decimal RequestedAmount { get; set; }
 
         public DateTime DateSubmitted { get; set; }
@@ -11,6 +12,9 @@
         public LoanApplicationStatus Status { get; set; }
 
         public int LoanTypeId { get; set; }
+
+        [ForeignKey(nameof(PeriodId))]
+        public InvestmentPeriod Period { get; set; }
 
         public string Details { get; set; }
         public virtual LoanType LoanType { get; set; }
