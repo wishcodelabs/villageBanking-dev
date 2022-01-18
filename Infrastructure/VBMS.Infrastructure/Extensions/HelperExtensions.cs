739a582acedbd5c335bf6fea9f3a432fccd7c695
Wisdom Jere <wisdomjere5@gmail.com>
2022-01-18 08:18:19 +0200

    Clean Up

--- Infrastructure/VBMS.Infrastructure/Extensions/HelperExtensions.cs
+++ Infrastructure/VBMS.Infrastructure/Extensions/HelperExtensions.cs
@@ -9,22 +9,20 @@ public static class HelperExtensions
     public static decimal GetTotalInterest(this Loan loan)
     {
         var totalInterest = 0.0;
+        var n = loan.InterestRate.PaybackDuration;
+        var p = (double)loan.ApprovedAmount;
+        var r = loan.InterestRate.InterestRate / 100;
         switch (loan.InterestRate.InterestType)
         {
             case InterestType.Simple:
-                totalInterest = (double)loan.ApprovedAmount
-                                * loan.InterestRate.InterestRate
-                                * Convert.ToDouble(loan.InterestRate.PaybackDuration) / 100;
+
+                totalInterest = p * r * n;
                 break;
             case InterestType.Compound:
-                {
-                    var n = loan.InterestRate.PaybackDuration;
-                    var p = (double)loan.ApprovedAmount;
-                    var r = loan.InterestRate.InterestRate / 100;
 
-                    totalInterest = p * (Math.Pow(1 + r, n) - 1);
-                    break;
-                }
+                totalInterest = p * (Math.Pow(1 + r, n) - 1);
+                break;
+
         }
 
         return (decimal)totalInterest;
