57e65feedd0d84becbd40f0ddf632466e3c4370b
Wisdom Jere <wisdomjere5@gmail.com>
2022-01-18 08:09:43 +0200

    refactor

--- Infrastructure/VBMS.Infrastructure/Extensions/HelperExtensions.cs
+++ Infrastructure/VBMS.Infrastructure/Extensions/HelperExtensions.cs
@@ -1,34 +1,33 @@
-﻿namespace VBMS.Infrastructure.Extensions
+﻿namespace VBMS.Infrastructure.Extensions;
+
+public static class HelperExtensions
 {
-    public static class HelperExtensions
+
+    public static decimal GetAmountOwing(this Loan loan)
+
+        => loan.GetTotalInterest() + loan.ApprovedAmount;
+    public static decimal GetTotalInterest(this Loan loan)
     {
-
-        public static decimal GetAmountOwing(this Loan loan)
-
-            => loan.GetTotalInterest() + loan.ApprovedAmount;
-        public static decimal GetTotalInterest(this Loan loan)
+        var totalInterest = 0.0;
+        switch (loan.InterestRate.InterestType)
         {
-            var totalInterest = 0.0;
-            switch (loan.InterestRate.InterestType)
-            {
-                case InterestType.Simple:
-                    totalInterest = (double)loan.ApprovedAmount
-                                    * loan.InterestRate.InterestRate
-                                    * Convert.ToDouble(loan.InterestRate.PaybackDuration) / 100;
+            case InterestType.Simple:
+                totalInterest = (double)loan.ApprovedAmount
+                                * loan.InterestRate.InterestRate
+                                * Convert.ToDouble(loan.InterestRate.PaybackDuration) / 100;
+                break;
+            case InterestType.Compound:
+                {
+                    var n = loan.InterestRate.PaybackDuration;
+                    var p = (double)loan.ApprovedAmount;
+                    var r = loan.InterestRate.InterestRate / 100;
+
+                    totalInterest = p * (Math.Pow(1 + r, n) - 1);
                     break;
-                case InterestType.Compound:
-                    {
-                        var n = loan.InterestRate.PaybackDuration;
-                        var p = (double)loan.ApprovedAmount;
-                        var r = loan.InterestRate.InterestRate / 100;
-
-                        totalInterest = p * (Math.Pow(1 + r, n) - 1);
-                        break;
-                    }
-            }
-
-            return (decimal)totalInterest;
-
+                }
         }
+
+        return (decimal)totalInterest;
+
     }
 }
